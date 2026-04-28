7c783ec0cbf591c65b4cb6fd892f3bb1899df452
Jonguk Cheong <jdal3031@snu.ac.kr>
2026-04-28 13:51:27 +0900

    Colors

--- configs/nvim/lua/plugins/themes/gruber-darker.lua
+++ configs/nvim/lua/plugins/themes/gruber-darker.lua
@@ -2,10 +2,14 @@ return {
 	"blazkowolf/gruber-darker.nvim",
 	opts = {},
 	config = function()
+		-- Apply the quartz override only when gruber-darker itself is active.
+		-- Previously `pattern = "*"` clobbered every other colorscheme's struct-
+		-- member highlights (including dalbit-light's #0e3a64 and dalbit-mono-light's
+		-- fg1 gray), making them all look like gruber-darker's quartz #95a99f.
 		local group = vim.api.nvim_create_augroup("CustomHighlights", { clear = true })
 		local quartz = "#95a99f"
 		vim.api.nvim_create_autocmd("ColorScheme", {
-			pattern = "*",
+			pattern = "gruber-darker",
 			group = group,
 			callback = function()
 				vim.api.nvim_set_hl(0, "@variable.member", { fg = quartz, bold = false })
